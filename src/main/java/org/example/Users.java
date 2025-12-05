cd91e114135c9752b641aeff5526fb2e96d4372a
619vinay <kanakamvinay47630@gmail.com>
2025-12-05 11:17:46 +0530

    another feature added

--- src/main/java/org/example/Users.java
+++ src/main/java/org/example/Users.java
@@ -15,6 +15,15 @@ public class Users
     private String name;
     private String mobile;
     private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public Long getUserid() {
         return userid;
