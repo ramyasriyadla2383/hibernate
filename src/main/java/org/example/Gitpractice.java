fec950f9706e8476f218e184a741afe0129b75db
YADLA RAMYASRI <ramyasriyadla2383@gmail.com>
2025-12-05 11:25:55 +0530

    added another class

---
+++ src/main/java/org/example/Gitpractice.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Gitpractice {
+    public static void main(String[] args) {
+        System.out.println("Gitpractice");
+        System.out.println("collobaration with other to access the  changes ");
+    }
+}
